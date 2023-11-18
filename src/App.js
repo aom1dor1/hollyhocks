bbe246630ff1b9accf121f547a364b3cc821f799
下山　碧 <135989997+aom1dor1@users.noreply.github.com>
2023-11-18 04:27:54 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -6,20 +5,26 @@ function App() {
     <div className="App">
       <header className="App-header bg-wed">
         <img
-          src="https://th.bing.com/th?id=OIP.m1dn7Ozxtio2ksWE-LM0FwHaH3&w=242&h=257&c=8&rs=1&qlt=90&o=6&dpr=1.4&pid=3.1&rm=2"
+          //src="https://th.bing.com/th?id=OIP.m1dn7Ozxtio2ksWE-LM0FwHaH3&w=242&h=257&c=8&rs=1&qlt=90&o=6&dpr=1.4&pid=3.1&rm=2"
           className="App-logo"
           alt="logo"
         />
-        <p className="text-wed-dark">
-          <code>WED</code> Community にようこそ
-        </p>
+        <p className="text-wed-dark">Artem にようこそ</p>
         <a
           className="text-wed-dark underline"
           href="https://wed.company/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn WED
+          教科書を出品する
+        </a>
+        <a
+          className="text-wed-dark underline"
+          href="https://wed.company/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          教科書を購入する
         </a>
       </header>
     </div>
